d737b03353c7712655b1b53f6dc0672ac1277c70
iqbalpa <iqbalpahlevi16@gmail.com>
2022-08-31 22:51:59 +0700

    menambahkan setting di settings.py

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k&-f4885oi$^tvk97*^z$w#)_o12z)vcain4^#kkqij!0-05^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,3 +121,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+MIDDLEWARE = [
+    ...,
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
